fae2703818dad7e28a1396581862429497015ca2
FelipeFerreira13 <felipeferreira1310@hotmail.com>
2024-05-09 03:04:08 -0400

    add Differential project

---
+++ DifferentialRobot/include/constants.h
@@ -0,0 +1,33 @@
+#ifndef CONST
+#define CONST
+
+static int ENCODER_BACK     = 0;
+static int ENCODER_RIGHT    = 1;
+static int ENCODER_LEFT     = 2;
+static int ENCODER_ELEVATOR = 3;
+
+static int MOTOR_BACK_0     = 19;
+static int MOTOR_BACK_1     = 18;
+static int MOTOR_RIGHT_0    = 17;
+static int MOTOR_RIGHT_1    = 16;
+static int MOTOR_ELEVATOR_0 = 15;
+static int MOTOR_ELEVATOR_1 = 14;
+static int MOTOR_LEFT_0     = 13;
+static int MOTOR_LEFT_1     = 12;
+
+static int SHARP_FRONT_LEFT  = 0;
+static int SHARP_FRONT_RIGHT = 1;
+static int SHARP_LEFT        = 2;
+static int SHARP_RIGHT       = 3;
+
+static int LIMITSWITCH_UP   = 8;
+static int LIMITSWITCH_DOWN = 9;
+
+#define PI 3.14159
+#define wheelRadius  6.25   //Wheel's radius [cm]
+#define frameRadius  28     //Robot's radius [cm]
+#define ticksPerRev  1464   //Encoder pulses
+
+#endif
+
+
