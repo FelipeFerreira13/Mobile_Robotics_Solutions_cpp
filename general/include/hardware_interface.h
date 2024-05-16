ea890c07d6979e2d2ffc7b7e3abfbc2599480c07
FelipeFerreira13 <felipeferreira1310@hotmail.com>
2024-05-16 11:31:21 -0400

    update motor driver pin

--- general/include/hardware_interface.h
+++ general/include/hardware_interface.h
@@ -5,21 +5,21 @@
 
 #include "constants.h"
 
-enum GRIPPER { GRIPPER_OPEN = 150, GRIPPER_CLOSE = 50 };
+enum GRIPPER { GRIPPER_OPEN = 50, GRIPPER_CLOSE = 150 };
 
 static int ENCODER_BACK     = 0;
 static int ENCODER_RIGHT    = 1;
 static int ENCODER_LEFT     = 2;
 static int ENCODER_ELEVATOR = 3;
 
+static int MOTOR_LEFT_0     = 21;
+static int MOTOR_LEFT_1     = 20;
 static int MOTOR_BACK_0     = 19;
 static int MOTOR_BACK_1     = 18;
 static int MOTOR_RIGHT_0    = 17;
 static int MOTOR_RIGHT_1    = 16;
 static int MOTOR_ELEVATOR_0 = 15;
 static int MOTOR_ELEVATOR_1 = 14;
-static int MOTOR_LEFT_0     = 13;
-static int MOTOR_LEFT_1     = 12;
 
 static int SHARP_FRONT_LEFT  = 0;
 static int SHARP_FRONT_RIGHT = 1;
@@ -32,7 +32,7 @@ static int LIMITSWITCH_LOW   = 9;
 static int BUTTON_START = 11;
 static int BUTTON_STOP  = 10;
 
-static int SERVO_GRIPPER = 20;
+static int SERVO_GRIPPER = 12;
 
 void delay( int ms );
 void delayMicroseconds( int us );
