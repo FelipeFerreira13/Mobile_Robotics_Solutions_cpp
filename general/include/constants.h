7a2b8cbbfc1f04eb996860cdedc9675c5ff43a7f
Felipe Ferreira <felipeferreira1310@hotmail.com>
2024-07-18 18:51:58 +0400

    Update constants.h

--- general/include/constants.h
+++ general/include/constants.h
@@ -3,7 +3,7 @@
 
 #define PI 3.14159
 #define wheelRadius  5.1   //Wheel's radius [cm]
-#define frameRadius  21     //Robot's radius [cm]
+#define frameRadius  21.5     //Robot's radius [cm]
 #define ticksPerRev  1464   //Encoder pulses
 
 #endif
