f8c2ceb004465ace85f359fecb6ffd8266a23954
FelipeFerreira13 <felipeferreira1310@hotmail.com>
2024-08-20 05:19:41 -1000

    add imu declaration

--- main.cpp
+++ main.cpp
@@ -16,37 +16,15 @@ int main(int argc, char *argv[])
 	//Initialize the VMX
 	VMXPi vmx(realtime, update_rate_hz);
 	vmx_ptr = &vmx;
-	
-    // initialize the HARDWARE
-	start_button.Init( BUTTON_START );
-	stop_button.Init( BUTTON_STOP );
 
-	limitswitch_low.Init( LIMITSWITCH_LOW);
-	limitswitch_high.Init( LIMITSWITCH_HIGH);
+	initHardware();
 
-	enc_left.Init( ENCODER_LEFT );
-	enc_right.Init( ENCODER_RIGHT );
-	enc_back.Init( ENCODER_BACK );
-	enc_elevator.Init( ENCODER_ELEVATOR );
-
-	gripper.Init( SERVO_GRIPPER );
-
-	sensor_front_left.Init( SHARP_FRONT_LEFT );
-	sensor_front_right.Init( SHARP_FRONT_RIGHT );
-	sensor_left.Init( SHARP_LEFT );
-	sensor_right.Init( SHARP_RIGHT );
-
-	motor_left.Init( MOTOR_LEFT_0, MOTOR_LEFT_1 );
-	motor_right.Init( MOTOR_RIGHT_0, MOTOR_RIGHT_1 );
-	motor_back.Init( MOTOR_BACK_0, MOTOR_BACK_1 );
-	motor_elevator.Init( MOTOR_ELEVATOR_0, MOTOR_ELEVATOR_1 );
-
-	motor_left.SetMotorPWM(0);
-	motor_right.SetMotorPWM(0);
-	motor_back.SetMotorPWM(0);
-	motor_elevator.SetMotorPWM(0);
+	AHRSCallback imu( vmx.ahrs );	// Initialize IMU
+	imu.Init();
 
 	OMS oms;	// Creates the OMS Object
+
+	
 
 	oms.setGripper( GRIPPER_CLOSE );	//Set the Gripper to open
 
@@ -54,6 +32,7 @@ int main(int argc, char *argv[])
 	while( start_button.Get() ){
 		delay(500);
 		printf("sharp: %f\n", sensor_front_left.GetIRDistance());
+
 	}	// Wait until the Start button is pressed
 
 	SetPosition( 0.0, 0.0, 0.0 );
@@ -62,7 +41,7 @@ int main(int argc, char *argv[])
 	oms.reset( -1 );	// Hit the LimitSwitch to reset the OMS position
 	oms.setGripper( GRIPPER_OPEN );	//Set the Gripper to open
 
-	// SetPosition( 30.0, 30.0, 0.0 );
+
 
 	return 0;
 
